29c1292286af64e62fee347dbbc2e5d56b249521
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2020-08-29 18:19:18 +0900

    Hom

---
+++ src/HomFunctors.hs
@@ -0,0 +1,25 @@
+module HomFunctors where
+
+{-
+class Functor f where
+    fmap :: (a -> b) -> f a -> f b
+
+instance Functor ((->) r) where
+    fmap = (.)
+
+instance Functor ((,) a) where
+    fmap f (x,y) = (x, f y)
+-}
+
+class Contra f where
+    pamf :: (a -> b) -> f b -> f a
+
+newtype Moh b a = Moh { getHom :: a -> b }
+
+instance Contra (Moh b) where
+    pamf f (Moh g) = Moh (g . f)
+
+newtype Riap b a = Riap { getPair :: (a,b) }
+
+instance Functor (Riap b) where
+    fmap f (Riap (x,y)) = Riap (f (x,y))
