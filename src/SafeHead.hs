805118addb2c899555b892b6b3ea4a7d9e3fd68d
Masahiro Yamauchi <sgt.yamauchi@gmail.com>
2020-09-02 21:47:48 +0900

    safehead

--- src/SafeHead.hs
+++ src/SafeHead.hs
@@ -2,6 +2,10 @@ module SafeHead where
 
 import Data.Functor.Const
 
+safehead :: [a] -> Maybe a
+safehead []     = Nothing
+safehead (x:xs) = Just x
+
 mylength :: [b] -> Const Int b
 mylength []     = Const 0
 mylength (x:xs) = Const (1 + getConst (mylength xs))
@@ -12,3 +16,10 @@ mylength (x:xs) = Const (1 + getConst (mylength xs))
 --
 -- >>> mylength [1,2,3]
 -- Const 3
+
+-- | vertical composition
+-- >>> safehead . concat . (map . map) length $ [["hello", "Bon jour"], ["Guten Tag"], ["abc", "def", "gh"]]
+-- Just 5
+--
+-- >>> fmap length . safehead . concat $ [["hello", "Bon jour"], ["Guten Tag"], ["abc", "def", "gh"]]
+-- Just 5
